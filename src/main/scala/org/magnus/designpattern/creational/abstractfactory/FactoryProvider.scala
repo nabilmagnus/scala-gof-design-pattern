15b69d37243a9e7b45f748a8469823cb33917c36
Nabil AMEZIANE <nameziane@tf1.fr>
2023-11-17 23:44:55 +0100

    ✨cleanup(dp/creational): format code

--- src/main/scala/org/magnus/designpattern/creational/abstractfactory/FactoryProvider.scala
+++ src/main/scala/org/magnus/designpattern/creational/abstractfactory/FactoryProvider.scala
@@ -7,6 +7,6 @@ object FactoryProvider {
   private val default: AbstractFactory = new CsvFactory
   private val factories: mutable.HashMap[String, AbstractFactory] = mutable.HashMap("json" -> new JsonFactory, "csv" -> default)
 
-  def factory = factories.getOrElse(System.getProperty("fileType"), default)
+  def factory: AbstractFactory = factories.getOrElse(System.getProperty("fileType"), default)
 }
 
